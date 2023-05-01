fe87ac650b845b5ac8ca964b134bf5a6dbb71743
BobWritesCode <warwick_hart@hotmail.com>
2023-05-01 21:14:03 +0100

    chore: remove unused line of code

--- user_notes/serializers.py
+++ user_notes/serializers.py
@@ -5,7 +5,6 @@ from user_notes.models import UserNote
 class UserNoteSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    # note_id = serializers.ReadOnlyField(source='owner.usernote.id')
 
     def get_is_owner(self, obj):
         request = self.context['request']
