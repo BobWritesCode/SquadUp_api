351b4478a856a2562d95fc01d2ae6aff49a20875
BobWritesCode <warwick_hart@hotmail.com>
2023-05-01 13:43:04 +0100

    improve: add natural time for created and updated

--- lfg_slots/serializers.py
+++ lfg_slots/serializers.py
@@ -1,10 +1,19 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from lfg_slots.models import LFG_Slot
 
 
 class LFG_SlotSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     def get_is_owner(self, obj):
         request = self.context['request']
