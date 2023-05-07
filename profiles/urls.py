1acb919f1a14e35eb7da0556b7f2163d94a31568
BobWritesCode <warwick_hart@hotmail.com>
2023-05-07 15:51:17 +0100

    chore: add url paths for profile views

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from profiles import views
+from .views import user_email, profile_details
 
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
-    path('profiles/<int:pk>/', views.ProfileDetail.as_view())
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
+    path('profiles/email/<int:user_id>/', user_email, name='user_email'),
+    path('profiles/details/<int:user_id>/', profile_details, name='profile_details'),
 ]
