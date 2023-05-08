6bc624c907b30bcea3e0412a90b2d40debe391c1
BobWritesCode <warwick_hart@hotmail.com>
2023-05-08 14:15:10 +0100

    feat: allow users to add/edit notes #21

--- user_notes/models.py
+++ user_notes/models.py
@@ -1,11 +1,29 @@
+from typing import Any
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class UserNote(models.Model):
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     target_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='target_user')
-    content = models.TextField(blank=True, max_length=200)
+    content = models.CharField(
+        blank=True,
+        max_length=200,
+        error_messages={
+            'max_length': "Max length is 200 character",
+        })
+
+    @classmethod
+    def create(cls, owner,target_user,content):
+        user_note = cls(owner=owner, target_user=target_user, content=content)
+        return user_note
+
+    def clean(self):
+        if len(self.content) > 200:
+            raise ValidationError(
+                {'content' : 'Max length is 200 characters.'}
+                )
 
     class Meta:
         ordering = ['-id']
--- user_notes/views.py
+++ user_notes/views.py
@@ -1,22 +1,19 @@
-from rest_framework import generics, permissions, filters
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions
 from squadup_api.permissions import IsOwnerOrReadOnly
 from .models import UserNote
 from .serializers import UserNoteSerializer
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.db.models import Q
+import bleach
+from django.core.exceptions import ValidationError
 
 
 class UserNoteList(generics.ListCreateAPIView):
     serializer_class = UserNoteSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = UserNote.objects.all().order_by('-id')
-    filter_backends = [
-    ]
-    filterset_fields = [
-    ]
-    search_fields = [
-    ]
-    ordering_fields = [
-    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -26,3 +23,87 @@ class UserNoteDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserNoteSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = UserNote.objects.annotate().order_by('-id')
+
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        target_user = User.objects.get(pk=pk)
+        query = Q(owner=request.user) & Q(target_user=target_user)
+        user_note = UserNote.objects.filter(query).first()
+        # If note object exists, return contents.
+        if user_note:
+            return JsonResponse({
+                'user_note': [user_note.content],
+            }, status=200)
+        # If note object does not exists, return blanks string.
+        return JsonResponse({
+            'user_note': [''],
+        }, status=200)
+
+    def put(self, request, *args, **kwargs):
+        # Use Bleach to remove malicious script tags
+        new_note = bleach.clean(request.data['userNote'])
+        # Get target user profile PK
+        pk = self.kwargs.get('pk')
+        # Search for user note object, if exists.
+        query = Q(owner=request.user.id) & Q(target_user=pk)
+        user_note = UserNote.objects.filter(query).first()
+        # If a user note object already exists, proceed to edit object.
+        if user_note:
+            user_note = get_object_or_404(UserNote, query)
+            user_note.content = new_note
+            try:
+                # Run clean method to check for errors.
+                user_note.clean()
+                # Update object.
+                user_note.save()
+
+            except ValidationError as err:
+                errorList = []
+                for e in err:
+                    errorList.append(e[1])
+                return JsonResponse({
+                    'non_field_errors': errorList,
+                }, status=400)
+
+            # Unexpected error has occurred and unique error code.
+            except Exception as err:
+                return JsonResponse({
+                    'non_field_errors': ['Unknown error (Aardwolf)'],
+                }, status=400)
+
+            # If no errors return saved string.
+            return JsonResponse({
+                'user_note': [new_note],
+            }, status=200)
+
+        # If user note object does not exist then create obj.
+        if not user_note:
+            target_user = User.objects.get(pk=pk)
+            try:
+                # Create instance of UserNote.
+                newUserNote = UserNote.create(request.user, target_user, new_note)
+                # Run clean method to check for errors.
+                newUserNote.clean()
+                # Save instance as object.
+                newUserNote.save()
+            except ValidationError as err:
+                errorList = []
+                for e in err:
+                    errorList.append(e[1])
+                return JsonResponse({
+                    'non_field_errors': errorList,
+                }, status=400)
+            # Unexpected error has occurred and unique error code.
+            except Exception as err:
+                return JsonResponse({
+                    'non_field_errors': ['Unknown error (Adelie Penguin)'],
+                }, status=400)
+
+            return JsonResponse({
+                'user_note': [new_note],
+                'response': ['Created new note.'],
+            }, status=200)
+
+        return JsonResponse({
+            'non_field_errors': ['Unknown error (Abyssinian Cat)'],
+        }, status=400)
