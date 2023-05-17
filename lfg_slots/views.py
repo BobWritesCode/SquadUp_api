c10375e636323865142d0c2cab9aa697c16ea49b
BobWritesCode <warwick_hart@hotmail.com>
2023-05-17 10:32:59 +0100

    chore: add filters to class

--- lfg_slots/views.py
+++ lfg_slots/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from squadup_api.permissions import IsOwnerOrReadOnly
 from .models import LFG_Slot
 from .serializers import LFG_SlotSerializer
@@ -8,14 +9,15 @@ class LFG_SlotList(generics.ListCreateAPIView):
     serializer_class = LFG_SlotSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = LFG_Slot.objects.all().order_by('-id')
+
     filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
     ]
-    filterset_fields = [
-    ]
-    search_fields = [
-    ]
-    ordering_fields = [
-    ]
+    filterset_fields = ['lfg']
+    search_fields = ['owner']
+    ordering_fields = []
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
