ca70b8c4328819a983a7f20c96df9af1d82e6dc9
BobWritesCode <warwick_hart@hotmail.com>
2023-05-20 13:25:28 +0100

    feat: #32 add new class IsGroupOwnerOrReadOnly
    - Checks to make sure the person reviewing an application is
    the group owner, otherwise read only.

--- squadup_api/permissions.py
+++ squadup_api/permissions.py
@@ -1,4 +1,7 @@
 from rest_framework import permissions
+from lfg.models import LFG
+from lfg_slots.models import LFG_Slot
+
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     '''
@@ -13,3 +16,20 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+
+class IsGroupOwnerOrReadOnly(permissions.BasePermission):
+    '''
+    Permission class.
+
+    Methods:
+        has_object_permission - Checks to make sure the person reviewing an
+        application is the group owner, otherwise read only.
+    '''
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        slot = LFG_Slot.objects.get(pk=obj.slot.id)
+        lfg = LFG.objects.get(pk=slot.lfg.id)
+        return lfg.owner == request.user
