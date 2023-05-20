1cb226ad5931bbf1449bf0c4cd18e15d5320d8b0
BobWritesCode <warwick_hart@hotmail.com>
2023-05-20 13:23:47 +0100

    chore: add docstring to IsOwnerOrReadOnly class

--- squadup_api/permissions.py
+++ squadup_api/permissions.py
@@ -1,6 +1,14 @@
 from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    '''
+    Permission class.
+
+    Methods:
+        has_object_permission - Checks to make sure the person accessing an
+        object is the owner, otherwise read only.
+    '''
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
